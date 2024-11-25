3a7570eb99178c84096a26f806929ffad67a1b15
Aanthord <michael.doran.808@gmail.com>
2024-11-25 11:50:01 +0100

    Update mdns_wrapper.c

--- csrc/mdns_wrapper.c
+++ csrc/mdns_wrapper.c
@@ -1,1 +1,12 @@
 
+#include "mdns_wrapper.h"
+
+// Wrapper for starting mDNS service
+void start_mdns_service(const char *service_name, const char *hostname, int port) {
+    mdns_start_service(service_name, hostname, port);
+}
+
+// Wrapper for stopping mDNS service
+void stop_mdns_service() {
+    mdns_stop_service();
+}
