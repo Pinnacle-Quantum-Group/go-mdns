9a71b0ae93dbd66ba81c251180858ff8caa4348a
Aanthord <michael.doran.808@gmail.com>
2024-11-25 11:49:37 +0100

    Update mdns_wrapper.h

--- csrc/mdns_wrapper.h
+++ csrc/mdns_wrapper.h
@@ -1,1 +1,11 @@
+#ifndef MDNS_WRAPPER_H
+#define MDNS_WRAPPER_H
+
+#include "mdns.h"
+
+// Wrapper functions for Go compatibility
+void start_mdns_service(const char *service_name, const char *hostname, int port);
+void stop_mdns_service();
+
+#endif
 
